77cb0682bfe624c3b031113f4f1318f185eefc00
Andreas Gerken <andi.gerken@gmail.com>
2017-02-09 12:27:50 +0100

    jetzt aber

--- homeostasis.py
+++ homeostasis.py
@@ -1,91 +1,109 @@
+from __future__ import print_function
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig = plt.figure()
-ax = plt.axes(xlim=(-2,2), ylim=(-2, 2))
-line, = ax.plot([], [], lw =2)
+ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2))
+line, = ax.plot([], [], lw=2)
 
 angle = 10
-angleSpeed =0.1
+angleSpeed = 0.5
 l = 1
 g = -0.01
 friction = 0.97
 motorTorque = 0.009
 
-A = 0
-C = 0
-h = 0.1 
-b = 0
+A = np.zeros([2, 1])
+b = np.zeros([2, 1])
+
+C = np.ones([1, 2])
+h = 0.1
+
 y = 0
 
 epsA = 0.0002
 epsC = 0.002
 
+
 def init():
-	line.set_data([], [])
-	angle = 20
-	angleSpeed = 0.1
-	l = 1.5
-	return line,
+    line.set_data([], [])
+    return line,
+
 
 def animate(i):
-	global angle, angleSpeed, A, C, h, b, y
+    global angle, angleSpeed, A, C, h, b, y
+    x = np.reshape([np.sin(angle), np.cos(angle)],[2,1])
+    print("x:", x)
 
-	x[0] = np.sin(angle)
-	x[1] = np.cos(angle)
+    # Feed forward model
 
-	## Feed forward model
+    if (i != 0):
+        xPred = np.dot(A, y) + b
+        xError = x - xPred
+        print("xError: ", xError)
 
-	if(i != 0):
-		xPred = A * y + b
-		xError = np.power(x - xPred,2)
-		print(xError)
-		
-		## Train Model
-		A += epsA * xError * y
-		b += epsA * xError
+        # xError = np.dot(xError.T, xError)
 
-		## Train Controller
-		C += epsC * A * (1 - np.power(np.tanh(C * x + h),2)) * xError * x
-		h += epsC * A * (1 - np.power(np.tanh(C * x + h),2)) * xError
+        # print("xError: ", xError)
 
-		print("A b C h:",A,b,C,h)
-	
-	## Control ##
+        # Train Model
+        A += epsA * xError * y
+        b += epsA * xError
 
-	# K(x) = tanh(Cx + h)
-	y = np.tanh(C * x + h)
-	print(y)
+        # Train Controller
+        z = np.dot(C, x) + h
+        g_z = 1 - np.power(np.tanh(z),2)
+        print("g_z:",g_z)
+
+        # TODO: hacky?
+        n = (np.dot(A, g_z) * xError).T
+        print("n:", n)
+
+        C += epsC * n *
+
+        C += epsC * A * (1 - np.power(np.tanh(C * x + h), 2)) * xError * x
+        h += epsC * A * (1 - np.power(np.tanh(C * x + h), 2)) * xError
+
+    #    print("A b C h:", A, b, C, h)
+
+    # Control ##
+
+    # K(x) = tanh(Cx + h)
+    # print(C.shape, x.shape, h.shape)
+    y = np.tanh(np.dot(C, x) + h)
+    print("y:", y)
+
+    # Dynamics model ##
+
+    # gravity
+    angleSpeed += np.cos(angle) * g
+
+    # motor Torque
+    #if (int(i / 200) % 2 == 0):
+    #    angleSpeed += 0.1
+    # else:
+    #	y = 0
+
+    angleSpeed += motorTorque * y[0][0]
+    # friction
+    angleSpeed *= friction
+
+    # calculate new position
+    angle += angleSpeed
+
+    line_x = np.zeros(2)
+    line_y = np.zeros(2)
+
+    print("angle:",angle)
+    line_x[1] = np.cos(angle) * l
+    line_y[1] = np.sin(angle) * l
+
+    line.set_data(line_x, line_y)
+    return line,
 
 
-	## Dynamics model ##
-	
-	# gravity
-	angleSpeed += np.cos(angle) * g
-
-	# motor Torque
-	if(int(i/200)%2==0):
-		y = 1
-	else:
-		y = 0
-	angleSpeed += motorTorque * y
-
-	# friction
-	angleSpeed *= friction
-
-	# calculate new position
-	angle += angleSpeed
-
-	line_x = np.zeros(2)
-	line_y = np.zeros(2)
-	line_x[1] = np.cos(angle) * l
-	line_y[1] = np.sin(angle) * l
-
-	line.set_data(line_x,line_y)
-	return line,
-
-anim = animation.FuncAnimation(fig, animate, init_func = init, frames = 400, interval = 20, blit = True)
-
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=400, interval=20, blit=True)
 
 plt.show()
